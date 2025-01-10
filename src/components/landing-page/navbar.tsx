5053d40d6bc25c5678ff4a0afe2491bcee0eb85b
Alvaro Rodriguez <alvarord519@gmail.com>
2025-01-09 21:42:42 -0500

    Enhance navbar with cart details and total price display; update placeholder product data for consistency

--- src/components/landing-page/navbar.tsx
+++ src/components/landing-page/navbar.tsx
@@ -5,12 +5,15 @@ import Link from "next/link";
 import {
   Sheet,
   SheetContent,
+  SheetFooter,
   SheetHeader,
   SheetTitle,
   SheetTrigger,
 } from "../ui/sheet";
 import { montserrat, raleway } from "@/lib/fonts";
 import useCartStore from "@/store/cart";
+import Image from "next/image";
+import { Button } from "../ui/button";
 
 export default function Navbar() {
   const links = [
@@ -30,6 +33,7 @@ export default function Navbar() {
 
   const totalItems = useCartStore((state) => state.getTotalItems());
   const cart = useCartStore((state) => state.items);
+  const totalPrice = useCartStore((state) => state.getTotalPrice());
 
   return (
     <>
@@ -55,21 +59,67 @@ export default function Navbar() {
           <Sheet>
             <SheetTrigger className="relative">
               <ShoppingCart className="w-5 h-5" />
-              <span className={`${montserrat.className} absolute rounded-full bg-primary text-background -top-2 -right-2 min-w-[18px] py-[1px] text-xs`}>
+              <span
+                className={`${montserrat.className} absolute rounded-full bg-primary text-background -top-2 -right-2 min-w-[18px] py-[1px] text-xs`}
+              >
                 {totalItems}
               </span>
             </SheetTrigger>
-            <SheetContent>
+            <SheetContent
+              className={`flex flex-col h-full ${montserrat.className}`}
+            >
               <SheetHeader>
-                <SheetTitle>Tu carrito</SheetTitle>
+                <SheetTitle className="font-sans text-2xl">
+                  Tu Carrito
+                </SheetTitle>
               </SheetHeader>
-              <div>
-                {cart.map((item) => (
-                  <div key={item.id}>
-                    <span>{item.name}</span>
-                  </div>
-                ))}
+
+              <div className="flex flex-col flex-1">
+                <div className="border-t border-foreground mt-6 flex-1 overflow-auto">
+                  {cart.map((item) => (
+                    <div key={item.id} className="flex gap-4 pt-3">
+                      <Image
+                        src={item.image}
+                        width={80}
+                        height={100}
+                        alt="img"
+                        className="object-cover aspect-[3/4]"
+                      />
+                      <div className="flex flex-col w-full">
+                        <div className="flex justify-between ">
+                          <Link href={`/product/${item.slug}`} className="hover:underline">
+                            <p className="font-semibold ">{item.name}</p>
+                          </Link>
+                          <p>S/{item.price}</p>
+                        </div>
+                        <div className="pt-2">
+                          <p className="text-xs text-muted-foreground">
+                            {item.color}
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                  ))}
+                </div>
               </div>
+
+              <SheetFooter className="mt-auto border-t-2 border-foreground">
+                <div className="flex flex-col w-full">
+                  <div className="flex items-center justify-between my-4">
+                    <p className="text-base font-semibold">Total:</p>
+                    <p className="text-base text-foreground font-medium">
+                      S/ {totalPrice}
+                    </p>
+                  </div>
+
+                  <Button
+                    size="lg"
+                    className="w-full bg-primary text-base text-background py-3 rounded-full"
+                  >
+                    Ir a Pagar
+                  </Button>
+                </div>
+              </SheetFooter>
             </SheetContent>
           </Sheet>
         </div>
